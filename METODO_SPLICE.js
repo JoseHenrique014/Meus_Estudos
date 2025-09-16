eb87a0915a00de48197dc40a4fa19efc30398480
JOSE HENRIQUE <carvalhocostajr@gmail.com>
2025-09-16 23:53:24 +0000

    METODO_SPLICE

--- METODO_SPLICE.js
+++ METODO_SPLICE.js
@@ -1,3 +1,8 @@
 //USANDO O MÉTODO SPLICE
 let alunos = ['Henrique', 'Raquel', 'Biatriz', 'Larissa'];
-console.log('Vetor Original: ', alunos);
+console.log('-----Vetor Original----- ');
+console.log(alunos)
+
+alunos.splice(1,1);
+console.log('-----Vetor após remover splice(1, 1)-----');
+console.log(alunos); 
